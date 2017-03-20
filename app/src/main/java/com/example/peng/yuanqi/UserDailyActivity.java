819adbdd23fe9559650169c639117d544e6148cd
kphf1995cm <1632904034@qq.com>
2017-03-20 23:40:52 +0800

    first change

---
+++ app/src/main/java/com/example/peng/yuanqi/UserDailyActivity.java
@@ -0,0 +1,16 @@
+package com.example.peng.yuanqi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by peng on 2017/3/14.
+ */
+
+public class UserDailyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_main);
+    }
+}

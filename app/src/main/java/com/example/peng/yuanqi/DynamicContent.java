da8eb2c47093f48cf04ff7ca1ba8bc853f4e211a
kphf1995cm <1632904034@qq.com>
2017-04-05 21:34:03 +0800

    change register xml

---
+++ app/src/main/java/com/example/peng/yuanqi/DynamicContent.java
@@ -0,0 +1,15 @@
+package com.example.peng.yuanqi;
+
+import java.util.ArrayList;
+
+/**
+ * Created by peng on 2017/4/4.
+ */
+
+public class DynamicContent {
+    public String account;
+    public String name;
+    public ArrayList<String> content;
+    public int likePersonNum=0;
+    public ArrayList<String> likePersonAccount;
+}
--- app/src/main/java/com/example/peng/yuanqi/MainActivity.java
+++ app/src/main/java/com/example/peng/yuanqi/MainActivity.java
@@ -18,31 +18,36 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     public static URL serverUrl;//服务器url
-    public boolean isWebConnected=false;//网络状态
-    public int userCurState=0;//0，未登录，1，已登录
-    public boolean isUserHeaderSetted=false;//用户头像是否已经被设置，头像存储在本地文件系统上
+    public boolean isWebConnected = false;//网络状态
+    public int userCurState = 0;//0，未登录，1，已登录
+    public boolean isUserHeaderSetted = false;//用户头像是否已经被设置，头像存储在本地文件系统上
     private int loginResult;//登录成功或失败的结果
-    EditText userAccount=null;
-    EditText userPassword=null;
-    Button logIn=null;
-    Button register=null;
+    EditText userAccount = null;
+    EditText userPassword = null;
+    Button logIn = null;
+    Button register = null;
+
     //public Drawable getLocalDrawable()｛
-         //    ImageView imageView=(ImageView)findViewById(R.id.user_header);
-            //Drawable mydrawable=(Drawable)imageView.getDrawable();
-              //    return mydrawable;
-            //｝;//从本地文件系统中取出头像
-    public int testUserAccountAndPassword(URL url,String account,String password){return 0;};//检测用户账号和密码是否存在及匹配,返回0，成功，1，账号不存在，2，密码错误
+    //    ImageView imageView=(ImageView)findViewById(R.id.user_header);
+    //Drawable mydrawable=(Drawable)imageView.getDrawable();
+    //    return mydrawable;
+    //｝;//从本地文件系统中取出头像
+    public int testUserAccountAndPassword(URL url, String account, String password) {
+        return 0;
+    }
+
+    ;//检测用户账号和密码是否存在及匹配,返回0，成功，1，账号不存在，2，密码错误
 
     public boolean isNetworkConnected(Context context) {
-        if (context != null) {
-            ConnectivityManager connectivityManager = (ConnectivityManager) context
-                    .getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            if (networkInfo != null) {
-                return networkInfo.isAvailable();
-            }
-        }
-        return false;
+        //if (context != null) {
+         //   ConnectivityManager connectivityManager = (ConnectivityManager) context
+          //          .getSystemService(Context.CONNECTIVITY_SERVICE);
+            //NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            //if (networkInfo != null) {
+             //   return networkInfo.isAvailable();
+           // }
+        //}
+        return true;
     }
 
     @Override
@@ -50,53 +55,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_welcome);
         //检测网络状态
-        isWebConnected=isNetworkConnected(MainActivity.this);
-        if(isWebConnected)
-        {
-            setContentView(R.layout.user_login);
-            userAccount=(EditText)findViewById(R.id.user_account);
-            userPassword=(EditText)findViewById(R.id.user_password);
-            logIn=(Button)findViewById(R.id.login_in);
-            register=(Button)findViewById(R.id.register);
-            if(isUserHeaderSetted)
+        isWebConnected = isNetworkConnected(MainActivity.this);
+        if (isWebConnected) {
+            if (userCurState == 0)//user didnot login
             {
-            //从本地文件系统中取出用户头像，将其替换
-            // Drawable localDrawable=getLocalDrawable();
-            // ImageView userHeader=(ImageView)findViewById(R.id.user_header);
-            // userHeader.setImageDrawable(localDrawable);
-            }
-            logIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loginResult=testUserAccountAndPassword(serverUrl,userAccount.getText().toString(),userPassword.getText().toString());
-                if(loginResult==0)
-                {
-                    Intent intent=new Intent(MainActivity.this,UserDailyActivity.class);
-                    startActivity(intent);
-                    userCurState=1;//修改用户状态，已登录
+                setContentView(R.layout.user_login);
+                userAccount = (EditText) findViewById(R.id.user_account);
+                userPassword = (EditText) findViewById(R.id.user_password);
+                logIn = (Button) findViewById(R.id.login_in);
+                register = (Button) findViewById(R.id.register);
+                if (isUserHeaderSetted) {
+                    //从本地文件系统中取出用户头像，将其替换
+                    // Drawable localDrawable=getLocalDrawable();
+                    // ImageView userHeader=(ImageView)findViewById(R.id.user_header);
+                    // userHeader.setImageDrawable(localDrawable);
                 }
-                else
-                {
-                    if(loginResult==1)//账号不存在
-                    {
-                        Toast.makeText(getApplicationContext(),"账号不存在",Toast.LENGTH_LONG).show();
-                        userAccount.clearComposingText();//清空账号输入框中文
+                logIn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        loginResult = testUserAccountAndPassword(serverUrl, userAccount.getText().toString(), userPassword.getText().toString());
+                        if (loginResult == 0) {
+                            Intent intent = new Intent(MainActivity.this, UserDailyActivity.class);
+                            startActivity(intent);
+                            userCurState = 1;//修改用户状态，已登录
+                        } else {
+                            if (loginResult == 1)//账号不存在
+                            {
+                                Toast.makeText(getApplicationContext(), "账号不存在", Toast.LENGTH_LONG).show();
+                                userAccount.clearComposingText();//清空账号输入框中文
+                            } else {
+                                Toast.makeText(getApplicationContext(), "密码不正确", Toast.LENGTH_LONG).show();
+                                userPassword.clearComposingText();//清空密码输入框中文
+                            }
+                        }
                     }
-                    else
-                    {
-                        Toast.makeText(getApplicationContext(),"密码不正确",Toast.LENGTH_LONG).show();
-                        userPassword.clearComposingText();//清空密码输入框中文
+                });
+                register.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                        startActivity(intent);
                     }
-                }
+                });
             }
-        });
-            register.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
-                    startActivity(intent);
-                }
-            });
+            else//直接切换到主页面
+            {
+                Intent intent = new Intent(MainActivity.this, UserDailyActivity.class);
+                startActivity(intent);
+            }
         }
     }
 
